170cfeda36b5f375163e5542131b0cf4b7514fa8
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-08 01:45:25 -0400

    types

---
+++ typescript_2.js
@@ -0,0 +1,11 @@
+var isCool = true;
+var age = 56;
+var eyeColor = 'brown';
+var favoriteQuote = "i'm not old, I'm only ".concat(age);
+var pets = ['cat', 'dog', 'pig'];
+var pets2 = ['lion', 'dragon', 'lizard'];
+var wizard = {
+    a: 'john'
+};
+var nulll = undefined;
+var no = null;
---
+++ typescript_2.ts
@@ -0,0 +1,16 @@
+let isCool: boolean = true
+
+var age: number = 56
+
+let eyeColor: string = 'brown'
+let favoriteQuote: string = `i'm not old, I'm only ${age}`
+
+let pets: string[] = ['cat', 'dog', 'pig']
+let pets2: Array<string> = ['lion', 'dragon', 'lizard']
+
+let wizard: object = {
+  a: 'john'
+};
+
+let nulll: undefined = undefined
+let no: null = null
