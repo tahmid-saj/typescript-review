bf72dbd56fd72dc899df1945b8e0d569379a02eb
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-08 01:11:19 -0400

    adding type

---
+++ typescript_1.js
@@ -0,0 +1,5 @@
+var sumab = function (a, b) {
+    return a + b;
+};
+var ans = sumab(4, 5);
+console.log(ans);
---
+++ typescript_1.ts
@@ -0,0 +1,8 @@
+const sumab = (a : number, b : number): number => {
+  return a + b;
+}
+
+const ans = sumab(4, 5);
+
+console.log(ans);
+
